380d3321c13108f9fd527f80ab576b330304cd83
printerscanner <abbeyyacoe@gmail.com>
2022-05-12 12:40:46 +0100

    auto commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import './App.css';
+import ImageMasonry from 'react-image-masonry';
+import logo from './LOGO/logo.svg';
 
-import Footer from'./Footer'
-import About from './About'
+var listOfImages =[];
 
+class App extends React.Component{
+    importAll(r) {
+        return r.keys().map(r);
+    }
+    componentWillMount() {
+        listOfImages = this.importAll(require.context('../UPLOAD_IMAGES/', false, /\.(png|jpe?g|svg)$/));
+    }
+    render(){
+        return(
+            <div>
+                <img class="logo" src={logo} />
+                <h1 className="product-intro">recent projects<span style={{color: "red"}}>.</span></h1>
+                <p>This is where you put a little blurb about why you're sharing these images.</p>
+                <br></br><br></br>
 
+                <div className="product-container">
+                        <ImageMasonry numCols={3} forceOrder={false} className="image-container">
+                        {
+                                listOfImages.map(
+                                (image, index) =>    <img className="tile" key={index} src={image} alt="info"></img>
+                                )
+                        }
+                        </ImageMasonry>
+                        </div>
+                        <div>
+                        <h6 class="right">Made by <a href="https://github.com/printerscanner">printer_scanner</a>.</h6></div>
 
-function App({ children }) {
-
-
-  return (
-
-  	<div className="app">
-        <main>
-        {children}
-     	 </main>
-      </div>
-  );
+                </div>
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
-import { mount, route, lazy } from 'navi'
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, View } from 'react-navi'
-import api from './api'
-import Products from './Products'
 
 import './index.css'
-
 import App from './App'
 
-const routes =
-  mount({
-    '/': route({
-      title: "",
-      getData: () => api.fetchProducts(),
-      view: <Products />,
-    }),
-    '/product': lazy(() => import('./product')),
-  })
-
-ReactDOM.render(
-  <Router routes={routes}>
-    <App>
-      <Suspense fallback={null}>
-        <View />
-       </Suspense>
-
-      </App>
-
-
-
-
-  </Router>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
